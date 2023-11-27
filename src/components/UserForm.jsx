bddc3795e2f1e30e4f5757ccdb9adadbe8cfed05
iosebkhe <ioseb.khetsuriani@yahoo.com>
2023-11-28 01:01:48 +0400

    fix for router

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import sectorService from '../services/sectors.js';
